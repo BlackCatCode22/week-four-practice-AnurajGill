99f36f3933f3e9e3434d14e56d34096741bbd3e7
AnurajGill <141672740+AnurajGill@users.noreply.github.com>
2023-08-28 11:37:11 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+
+   // for(int i =0; i < 5; i++)
+    {
+       // cout << "i is " << i;
+    }
+
+    //int i = 0;
+
+   // while(i < 100000)
+    {
+
+        //cout << i;
+        //i++;
+        //system("pause");
+    }
+
+
+    //string myStr;
+
+    //myStr = "abcdefghij";
+
+    //string mySubString = myStr.substr (0, 20);
+   // for(int i = 0; i < 10; i++){
+
+        //cout << myStr.at(i) << "\n";
+    //}
+
+
+    //for (int i = 9; i >= 0; i--){
+
+        //cout << myStr.at(i) << "\n";
+   // }
+
+    //ChatGPT
+
+    //for (char c = 'a'; c <= 'j'; ++c){
+        //cout << c;
+    //}
+    //cout << endl;
+
+    char myChars[10] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',};
+
+
+    for (int i = 0; i < 10; i++){
+        cout << "myChars[" << i << "] is " << myChars[i] << "\n\n";
+    }
+
+    int num1 = 100;
+    int num2 = 33;
+
+    if( num1 > num2){
+        cout << "\n The text condition was true";
+    }
+        else{
+            cout << "\n The test condition evaluated to false";
+        }
+    return 0;
+
+
+
+}
